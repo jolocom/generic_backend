5330a648a4377b27e18905873b5d6fc948b2b9a0
Exulansis <rusu.eugeniu@gmx.de>
2019-02-17 22:45:29 +0100

    Set up folder structure for custom middleware + routes

--- src/customHandlers/customRoutes.ts
+++ src/customHandlers/customRoutes.ts
@@ -1,46 +1,5 @@
-import axios from 'axios'
 import { RedisApi } from 'src/types';
-import { Response, Request, Express } from 'express';
-import { debugConfig, gatewayUrl, AccessTokens, generateHeadersFromAccessTokens } from './utils';
+import { Express } from 'express';
 
-const spendUserCheque = (
-    accessTokens: AccessTokens,
-    gatewayUrl: string
-) => (
-    cardNumber: number,
-    chequeId: number
-) => {
-    axios.post(`${gatewayUrl}/redeem`, {
-        cardNumber,
-        chequeIds: [chequeId]
-    }, {
-        headers: generateHeadersFromAccessTokens(accessTokens)
-    })
-}
-
-const configuredSpendUserCheque = spendUserCheque(
-    debugConfig,
-    gatewayUrl
-)
-
-export const configureCustomRoutes = (
-    app: Express,
-    redis: RedisApi
-) => {
-    app.post("/spend/:chequeId", async (req: Request, res: Response) => {
-        const { chequeId } = await req.params;
-        const aKaartIdentifier = await redis.getAsync(chequeId);
-
-        if (!aKaartIdentifier) {
-            return res.status(401).send("Attempt to spent unknown cheque");
-        }
-
-        try {
-            await configuredSpendUserCheque(+aKaartIdentifier, +chequeId);
-            await redis.delAsync(chequeId);
-            return res.status(200).send(chequeId);
-        } catch (err) {
-            return res.status(500).send(err.message);
-        }
-    });
+export const configureCustomRoutes = (app: Express, redis: RedisApi) => {
 }
