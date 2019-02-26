cd997c031a135e2c25a6d459fca792d09cea4e7f
Exulansis <rusu.eugeniu@gmx.de>
2019-02-26 14:40:21 +0100

    Restored AKaart specific custom ligc

--- src/customHandlers/customMiddleware.ts
+++ src/customHandlers/customMiddleware.ts
@@ -1,10 +1,77 @@
+import axios, { AxiosPromise } from 'axios'
 import { RequestWithInteractionTokens, RedisApi } from 'src/types'
 import { Response, NextFunction } from 'express'
+import { CredentialResponse } from 'jolocom-lib/js/interactionTokens/credentialResponse'
+import { claimsMetadata } from 'cred-types-jolocom-demo'
+import {
+  AccessTokens,
+  AKaartUserRecord,
+  generateHeadersFromAccessTokens,
+  debugConfig,
+  gatewayUrl
+} from './utils'
+import { areArraysEqual } from 'src/helpers/validators'
+
+const getUserDataALoyaltyService = (
+  accessTokens: AccessTokens,
+  gatewayUrl: string
+) => (identifier: string): AxiosPromise<AKaartUserRecord> => {
+  const endpoint = `${gatewayUrl}/account-info/card-numbers/${identifier}`
+  const headers = generateHeadersFromAccessTokens(accessTokens)
+  return axios.get<AKaartUserRecord>(endpoint, { headers })
+}
+
+const getUserInfo = getUserDataALoyaltyService(debugConfig, gatewayUrl)
+
+const getUserCheques = (cardNumber: string) =>
+  getUserInfo(cardNumber).then(res => res.data.cheques || [])
 
 export const addCustomAuthnMiddleware = (redis: RedisApi) => async (
   req: RequestWithInteractionTokens,
   res: Response,
   next: NextFunction
 ) => {
-  next()
+  const { suppliedCredentials } = req.userResponseToken
+    .interactionToken as CredentialResponse
+
+  const aKaartCredential = suppliedCredentials.find(credential =>
+    areArraysEqual(credential.type, claimsMetadata.akaart.type)
+  )
+
+  if (!aKaartCredential) {
+    res.status(401).send('No A-Kaart credential was found')
+  }
+
+  const { identifier: aKaartIdentifier } = aKaartCredential.claim
+
+  const aKaartRecord = {
+    success: true,
+    identifier: aKaartIdentifier,
+    cheque: {}
+  }
+
+  try {
+    const cheques = await getUserCheques(aKaartIdentifier)
+
+    if (cheques.length) {
+      req.middlewareData = {
+        ...aKaartRecord,
+        cheque: cheques[0]
+      }
+
+      await redis.setAsync(
+        cheques[0].chequeId.toString(),
+        aKaartIdentifier as string
+      )
+    }
+
+    next()
+  } catch (err) {
+    req.middlewareData = {
+      ...aKaartRecord,
+      success: false,
+      error: err.message
+    }
+    res.status(401).send('Could not validate A-Kaart account')
+  }
 }
